6142b30fcf918311ec33050d8559784058b44d4f
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-04-10 20:08:07 +0900

    ヴァリデーションの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
+  validates :last_name, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end

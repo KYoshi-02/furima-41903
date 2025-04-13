410e42960b2164e067faa6c4bc5383c33f929811
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-04-13 17:09:44 +0900

    マイグレーション実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :item_status_id, presence: true
+  validates :item_category_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :image, presence: true
+  validates :user, presence: true
+  validates :price, presence: true
 end

1968a7062abcc971016b173328be5c231b536678
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-04-14 22:52:39 +0900

    model修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,16 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_description, presence: true
-  validates :item_status_id, presence: true
-  validates :item_category_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :prefecture_id, presence: true
+  validates :item_status_id, numericality: { other_than: 1 }
+  validates :item_category_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
   validates :image, presence: true
   validates :user, presence: true
-  validates :price, presence: true
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
 end

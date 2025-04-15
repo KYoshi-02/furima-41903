386600ca182aef04e184628d007b9f9990e8adb4
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-04-15 22:47:03 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Furima41903
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
     config.autoload_paths += %W(#{config.root}/app/models/active_hash)
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

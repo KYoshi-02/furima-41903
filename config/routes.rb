22019cf6da3df26f78785b1d63c7d9d1f51daa88
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-04-21 19:06:41 +0900

    ordersコントローラー・Payjp記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
